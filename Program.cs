888590df06202eb5dcb37ef233bc52902530b0a2
Arthur Leonardo <arthurleonardo801@hotmail.com>
2021-07-31 19:13:16 -0300

    Soma nota

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomaNota
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor da nota 1: ");
+            double nota1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o valor da nota 2: ");
+            double nota2 = Convert.ToDouble(Console.ReadLine());
+            double soma = nota1 / nota2;
+            Console.WriteLine("A divisão notas é:" + soma);
+
+        }
+    }
+}
